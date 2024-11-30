51771b6eb04bbd48d652d5b3eee1af7448749d3a
syedabdulrehman1 <fast33210@gmail.com>
2024-12-01 02:10:14 +0500

    main.ts fixes for vercel

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,37 @@
 import * as dotenv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+// import * as express from 'express';
+import express from 'express';
+
 dotenv.config();
-// const app = await NestFactory.create(AppModule);
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  try {
+    const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
-  app.enableCors({
-    origin: 'http://localhost:3000',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
-  await app.listen(8001);
+    app.enableCors({
+      origin: process.env.FRONT_END_URL, // Adjust this if your frontend is hosted elsewhere
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      credentials: true,
+    });
+
+    // Initialize NestJS and start the Express server
+    await app.init();
+
+    // Listen on the port specified in environment variables, or use a default port
+    const port = process.env.PORT || 3001;
+    server.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.error('Error during app initialization:', error);
+    process.exit(1);
+  }
 }
+
 bootstrap();
+
+export default server;
